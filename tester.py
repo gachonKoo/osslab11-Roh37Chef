974aede552986a05208ef29c74d1241a4c891114
HanEarl <112889728+Roh37Chef@users.noreply.github.com>
2024-11-17 18:01:41 +0900

    lab11_2

--- tester.py
+++ tester.py
@@ -3,10 +3,15 @@ sys.path.append(".")
 
 import geo.utils as utils
 
+# Check available functions
+print(dir(utils))
+
+# Test pythagoras
 a, b = 3, 4
 c = utils.pythagoras(a, b)
 print('c =', c)
 
+# Test circle
 r = 10
 area = utils.circle(r)
 print('area =', area)
